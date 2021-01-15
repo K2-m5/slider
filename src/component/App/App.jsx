6840c608cbb67813f2cc02cb9528c3236fe8e891
Pavel Svirid <56111100+K2-m5@users.noreply.github.com>
2021-01-15 14:56:56 +0300

    Init test task

---
+++ src/component/App/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Swiper from '../Swiper/Swiper';
+
+const App = () => (
+  <div>
+    <Swiper />
+  </div>
+);
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './component/App/App';
+
+import './style.less';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<App />, rootElement);
