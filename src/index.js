1744363a28a0398e3dbf21f20c9452d731ffbda9
Pavel Svirid <56111100+K2-m5@users.noreply.github.com>
2021-02-03 14:11:43 +0300

    Create master branch and clear it

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './component/App/App';
-
-import './style.less';
-
-const rootElement = document.getElementById('root');
-
-ReactDOM.render(<App />, rootElement);
