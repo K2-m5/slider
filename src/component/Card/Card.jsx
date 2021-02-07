10c85e39c3ad5484881089a0d2e7e6857d2eb020
Pavel Svirid <56111100+K2-m5@users.noreply.github.com>
2021-02-07 16:37:10 +0300

    Revert "Create master branch and clear it"
    This reverts commit 1744363a28a0398e3dbf21f20c9452d731ffbda9.

---
+++ src/component/Card/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => (
+  <div>
+    <div>Header</div>
+    <div>Body</div>
+    <div>Footer</div>
+  </div>
+);
+
+export default Card;
---
+++ src/component/Swiper/Swiper.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from '../Card/Card';
+
+const Swiper = () => (
+  <div>
+    <Card />
+  </div>
+);
+
+export default Swiper;
